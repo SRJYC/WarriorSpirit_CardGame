5193b6c2c13f9266d2e6767fa9261dc9917b3115
SRJYC <you0rock0@gmail.com>
2019-07-31 15:33:14 -0300

    new texture

--- Assets/Scripts/Manager/ParticleManager/ParticleManager.cs
+++ Assets/Scripts/Manager/ParticleManager/ParticleManager.cs
@@ -14,12 +14,9 @@ public class ParticleManager : SingletonScriptableObject<ParticleManager>
     public ParticleType reference;
     public List<GameObject> particlePrefab;
 
-    public Vector3 scale = new Vector3(100, 100, 100);
     public void PlayEffect(ParticleType type, GameObject target)
     {
         GameObject particle = Instantiate(particlePrefab[(int)type]);
         particle.transform.position = target.transform.position;
-        particle.transform.localScale = scale;
-        //particle.GetComponent<ParticleSystem>().Play();
     }
 }
