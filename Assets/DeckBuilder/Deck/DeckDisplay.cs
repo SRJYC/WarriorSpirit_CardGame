84a35e8d4defd5315cde93be9664a87199ec30a3
SRJYC <you0rock0@gmail.com>
2019-08-05 18:56:54 -0300

    can change deck

---
+++ Assets/DeckBuilder/Deck/DeckDisplay.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeckBuilder
+{ 
+    public class DeckDisplay : MonoBehaviour
+    {
+        public CardCollection deck;
+
+        public SingleCardDisplay warriorDisplay;
+        public List<SingleCardDisplay> cardDisplays;
+
+        public bool hasChange = false;
+
+        public const int CopyLimit = 3;
+
+        public void Start()
+        {
+            for (int i = 0; i < cardDisplays.Count; i++)
+            {
+                cardDisplays[i].Init(this, i);
+            }
+        }
+
+        public void ChangeDeck(CardCollection cc)
+        {
+            //if (hasChange)
+            //    return;
+
+            Display(cc);
+        }
+
+        private void Display(CardCollection cc)
+        {
+            deck = cc;
+
+            warriorDisplay.Display(deck.m_Warrior);
+
+            for (int i = 0; i < cardDisplays.Count; i++)
+            {
+                cardDisplays[i].Display(deck.m_CardList[i]);
+            }
+        }
+
+        public void ChangeCard(int index, UnitData data)
+        {
+            if(index < 0 && data.IsWarrior)
+            {
+                deck.m_Warrior = data;
+                warriorDisplay.Display(data);
+
+                hasChange = true;
+            }
+            else if(NumOfCard(data) < CopyLimit)
+            {
+                deck.m_CardList[index] = data;
+                cardDisplays[index].Display(data);
+
+                hasChange = true;
+            }
+        }
+
+        private int NumOfCard(UnitData data)
+        {
+            int n = 0;
+            foreach(UnitData card in deck.m_CardList)
+            {
+                if (card == deck)
+                    n++;
+            }
+
+            return n;
+        }
+
+
+        [ContextMenu("Test")]
+        public void Test()
+        {
+            Display(deck);
+        }
+    }
+}
---
+++ Assets/DeckBuilder/Deck/SingleCardDisplay.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DeckBuilder
+{
+    public class SingleCardDisplay : MonoBehaviour
+        ,IDropHandler
+    {
+        public CardDisplay cardDisplay;
+
+        private int index;
+        private DeckDisplay deck;
+
+        public void Init(DeckDisplay dd, int i)
+        {
+            deck = dd;
+            index = i;
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            UnitData data = eventData.pointerDrag.GetComponent<CardDisplay>().m_CardData;
+
+            deck.ChangeCard(index, data);
+        }
+
+        public void Display(UnitData data)
+        {
+            if (data == null)
+                cardDisplay.gameObject.SetActive(false);
+            else
+            {
+                cardDisplay.Display(data);
+                cardDisplay.gameObject.SetActive(true);
+            }
+        }
+    }
+}
+
