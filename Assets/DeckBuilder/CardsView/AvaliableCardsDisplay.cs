b2ccdd8e1e0988879ea0ea27e26fc49ea3ea7115
SRJYC <you0rock0@gmail.com>
2019-08-05 15:39:16 -0300

    Deck Builder In Progress
    1.Display
    2.Text Label
    3.Intro

---
+++ Assets/DeckBuilder/CardsView/AvaliableCardsDisplay.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeckBuilder
+{
+    public class AvaliableCardsDisplay : MonoBehaviour
+    {
+        public GameObject m_Prefab;
+        public CardCollection m_Spirits;
+        public CardCollection m_Warriors;
+
+        public GameObject m_Container;
+
+        public bool isDisplaySpirit;
+        public ObjectPool m_Pool;
+
+        private void Start()
+        {
+            m_Pool = new ObjectPool(m_Prefab);
+            Display();
+        }
+
+        private void OnDestroy()
+        {
+            m_Pool.Clear();
+        }
+
+        public void Display()
+        {
+            m_Pool.DeactivateAll(true);
+
+            CardCollection cards = isDisplaySpirit ? m_Warriors : m_Spirits;
+
+            isDisplaySpirit = !isDisplaySpirit;
+
+            foreach(UnitData data in cards.m_CardList)
+            {
+                GameObject go = m_Pool.Get(true);
+
+                go.GetComponent<CardDisplay>().Display(data);
+
+                go.transform.SetParent(m_Container.transform);
+            }
+        }
+    }
+}
+
+
---
+++ Assets/Text/DisplayText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText : MonoBehaviour
+{
+    public List<TextProperty> m_label;
+    public string m_conntector;
+    public TMPro.TextMeshProUGUI m_Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string s = "";
+        ParseConnector();
+
+        foreach (TextProperty property in m_label)
+        {
+            s += property.ToString() + m_conntector;
+        }
+
+        m_Text.text = s;
+    }
+
+    private void ParseConnector()
+    {
+        m_conntector = m_conntector.Replace("\\n", "\n");
+        m_conntector = m_conntector.Replace("\\t", "\t");
+    }
+}
